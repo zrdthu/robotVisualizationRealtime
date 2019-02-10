3194dbb8c126d7d41c52f5da265ef5b2d3df4a02
Seyun <sk956@cornell.edu>
2019-02-10 13:18:37 -0500

    Fully animated

--- oriTrakHAR-master/robotVisualizationRealtime/src/app/services/sock.service.ts
+++ oriTrakHAR-master/robotVisualizationRealtime/src/app/services/sock.service.ts
@@ -22,11 +22,51 @@ export class SockService {
      return keys;
   }
 
+  function getQuaternionProduct (r, q) {
+    // ref: https://www.mathworks.com/help/aeroblks/quaternionmultiplication.html
+    return {
+      w: r.w * q.w - r.x * q.x - r.y * q.y - r.z * q.z,
+      x: r.w * q.x +  r.x * q.w - r.y * q.z + r.z * q.y,
+      y: r.w * q.y + r.x * q.z + r.y * q.w - r.z * q.x,
+      z: r.w * q.z - r.x * q.y + r.y * q.x + r.z * q.w
+    }
+  }
+
+  var initquat = {
+    w: 0.1604,
+    x : 0.5975, 
+    y: 0.0512, 
+    z : 0.784 
+  }
+
+  function q12q2 (q1, q2) {
+    return getQuaternionProduct( q1, getInverseQuaternion(q2))
+  }
+
+  function getInverseQuaternion (quat) {
+    // ref: https://www.mathworks.com/help/aeroblks/quaternioninverse.html?s_tid=gn_loc_drop
+    const denominator = quat.w * quat.w + quat.x * quat.x + quat.y * quat.y + quat.z * quat.z
+    return {
+      w: quat.w / denominator,
+      x: -quat.x / denominator,
+      y: -quat.y / denominator,
+      z: -quat.z / denominator
+    }
+  }
+
+  // calculate from the torso at this point 
+
+
+
   var keyarray = keys(data); 
   var delay = 20 //This is in ms
   var index = 0
   var interval = setInterval(() => {
+    // calculating the relative orientation  
+    // there are different kinds of ideas 
 
+// another code that will handle relative I'm gonna try both situations to see whether it would work or not. 
+ //realative torso for the calculations 
   newDataHandle('torso', data[keyarray[index]]);
     index += 1
     if (index == keyarray.length) {
@@ -36,8 +76,8 @@ export class SockService {
 
   function newDataHandle(msg, datainput) {
     dataModel.status[msg].quaternion.w = datainput['w']
-    dataModel.status[msg].quaternion.x = datainput['y'] 
-    dataModel.status[msg].quaternion.y = datainput['z'] 
+    dataModel.status[msg].quaternion.x = datainput['z'] 
+    dataModel.status[msg].quaternion.y = datainput['y'] 
     dataModel.status[msg].quaternion.z = datainput['x']
     }
    }
