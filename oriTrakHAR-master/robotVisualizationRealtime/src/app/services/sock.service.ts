69bbecdefd48ef6fb8a5ac51011779246d988c97
Seyun <sk956@cornell.edu>
2019-03-03 14:57:39 -0500

    progress elbow match

--- oriTrakHAR-master/robotVisualizationRealtime/src/app/services/sock.service.ts
+++ oriTrakHAR-master/robotVisualizationRealtime/src/app/services/sock.service.ts
@@ -7,7 +7,8 @@ import { DataModelService } from './data-model.service';
 import * as rightdata from './rightarm227botharms.json';
 import * as data from './torsomoving227botharms.json'; 
 import * as leftdata from './leftarm227botharms.json';
-//import * as elbow from './224elbowthird.json';
+import * as elbow from './227relativeelbowbotharms102.json'; 
+//import * as elbow from './224elbowthirFmad.json';
 
 
 @Injectable()
@@ -60,7 +61,7 @@ export class SockService {
   // calculate from the torso at this point 
 
   // var right key data 
-  //var elbowkeyarray = keys(elbow); 
+  var elbowkeyarray = keys(elbow); 
   var leftkeyarray = keys(leftdata);
   var rightkeyarray = keys(rightdata);
   var keyarray = keys(data); 
@@ -68,12 +69,12 @@ export class SockService {
   var index = 0
   var interval = setInterval(() => {
 
- console.log(rightdata[rightkeyarray[index]]);
- console.log("right data "); 
- console.log(data[keyarray[index]]); 
+ // console.log(rightdata[rightkeyarray[index]]);
+ // console.log("right data "); 
+ // console.log(data[keyarray[index]]); 
  //console.log(elbow[elbowkeyarray[index]]); 
-  // newDataHandle('rightUpper', elbow[elbowkeyarray[index]]);
-  newDataHandle('rightUpper', rightdata[rightkeyarray[index]]);
+   newDataHandle('rightUpper', elbow[elbowkeyarray[index]]);
+  //newDataHandle('rightUpper', rightdata[rightkeyarray[index]]);
    newDataHandle('rightLower', rightdata[rightkeyarray[index]]);
    newDataHandle('leftUpper', leftdata[leftkeyarray[index]]); 
    newDataHandle('leftLower', leftdata[leftkeyarray[index]]);
